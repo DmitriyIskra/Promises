656895d144bdbc2adbd1d4dd66111aea87d44abe
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-04-06 00:15:12 +0300

    Initial commit

---
+++ src/app/app.js
@@ -0,0 +1,26 @@
+import read from './reader'
+import json from './parser'
+import GameSavingLoader from './loader';
+
+const gameSavingLoader = new GameSavingLoader()
+
+const r = gameSavingLoader.load()
+    .then((saving) => {
+        saving = read();
+        return saving;
+    })
+    .then((saving) => {
+        const jsonString = json(saving);
+        return jsonString;
+    })
+    .then((data) => {
+        gameSavingLoader.gameSaving = JSON.parse(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log(gameSavingLoader.gameSaving)
+    })
+    
+
